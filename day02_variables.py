42b948138afd2487a1dd99b1ae5136b611e6a895
kalranamagar123-gif <kalranamagar123@gmail.com>
2026-01-12 22:21:43 -0500

    Day 02: Variables reassignment and memory reference
    Initialize variables for demonstration of memory reference.

---
+++ day02_variables.py
@@ -0,0 +1,18 @@
+#Variables 
+#Authur: Kalpana
+#Goal: Understand variables and memory reference
+
+name = "Ram"
+age = 20
+weight = 65.6
+
+age = 23
+
+a = 1
+b = a
+a = 2
+print(a)
+print(b)
+print(name)
+print(age)
+print(weight)
