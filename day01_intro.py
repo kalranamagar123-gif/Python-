22875ae7bcbeb8e30588f09523debbc115d7e853
kalranamagar123-gif <kalranamagar123@gmail.com>
2026-01-12 22:19:04 -0500

    Add introductory content to day01_intro.py

---
+++ day01_intro.py
@@ -0,0 +1,16 @@
+# day 01 -Introduction to python
+#Authur - Kalpana
+#Goal - Understand what python is and how it works 
+print("Python is high level programming language")
+print("Python is interpreted and runs line by line")
+print("Python is dynamically typed")
+print("Python manages memory automatically")
+print("Python is used in web development, AL and automation")
+
+"""
+Key concept study today:-
+- Programming language as communication with computers
+- high level and low level language
+- Interpredexecution model 
+- Dynamic typing 
+"""
