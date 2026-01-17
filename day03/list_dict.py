dedf01c47586d8404e78f9002d14d2916078368b
kalranamagar123-gif <kalranamagar123@gmail.com>
2026-01-16 23:42:20 -0500

    list and dictionary

---
+++ day03/list_dict.py
@@ -0,0 +1,23 @@
+name = "Kaile"
+age = 20
+country = "USA"
+is_student = True
+is_married = False
+print(type(name))
+print(type(age))
+print(type(country))
+print(type(is_student))
+print(type(is_married))
+
+foods = ["MO:MO", "Chicken Curry", "Chowmin", "Pizza", "Laughing"]
+print(foods[0])
+print(foods[4])
+
+profile = {
+    "name" : "Karen",
+    "age" : 23,
+    "country" : "USA"
+}
+print(profile["name"])
+print(profile["age"])
+print(profile["country"])
