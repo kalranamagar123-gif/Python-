941bbeaa99cfdc9f649109f794c35cacb4271f47
kalranamagar123-gif <kalranamagar123@gmail.com>
2026-01-16 23:36:09 -0500

    Python data types
    Added examples of various data types in Python including integer, float, string, boolean, list, tuple, dictionary, and set.

---
+++ day03_data_type.py
@@ -0,0 +1,40 @@
+#Integer
+age = 20
+year = 2005
+print(age)
+
+#Float
+height = 4.11
+weight = 45.89
+print(height)
+
+#string
+name = "Kaylee"
+country = "USA"
+print(name)
+
+#Boolean 
+is_student = True #Used for condition (if, while)
+is_married = False
+print(is_married)
+
+#List
+students = ["Rodha", "Rita", "Roni"] #Changeable collection
+marks = [78, 56, 34]
+print(marks[0])
+
+#Tuple 
+animal = ("cat", "dog") #Used when data should not be changed
+print(animal[-1])
+
+#dict(dictionary)
+student = {
+    "name" : "Kali",
+    "age" : 24,
+    "grade" : 4 
+}
+print(student["name"])
+
+#Set
+numbers = {1, 2, 3, 4, 5, 5}
+print(numbers) #no duplicates
